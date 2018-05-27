ffa12a9a596cb788e9084ebd44fc42456cf2c099
Dedek Julianto <dedekproject@gmail.com>
2018-05-28 01:24:18 +0700

    join

--- table.php
+++ table.php
@@ -2,12 +2,12 @@
   <div id="tipe-table">
     <ul>
       <?php
-        $query = mysqli_query($koneksi, "SELECT * FROM tipe_meja WHERE status='on'");
+        $query = mysqli_query($koneksi, "SELECT * FROM tipe_meja");
         while ($row=mysqli_fetch_assoc($query)) {
           if ($tipe_id == $row['tipe_id']) {
-            echo "<li><a href='".BASE_URL."index.php?tipe_id=$row[tipe_id]' class='active'>$row[tipe]</a></li>";
+            echo "<li><a href='".BASE_URL."index.php?tipe_id=$row[tipe_id]' class='active'>Room $row[tipe]</a></li>";
           } else {
-            echo "<li><a href='".BASE_URL."index.php?tipe_id=$row[tipe_id]'>$row[tipe]</a></li>";
+            echo "<li><a href='".BASE_URL."index.php?tipe_id=$row[tipe_id]'>Room $row[tipe]</a></li>";
           }
 
         }
@@ -21,9 +21,9 @@
     <ul>
       <?php
         if ($tipe_id) {
-          $query = mysqli_query($koneksi, "SELECT * FROM meja WHERE status='on' AND tipe_id='$tipe_id'");
+          $query = mysqli_query($koneksi, "SELECT meja.*, tipe_meja.tipe FROM meja JOIN tipe_meja ON meja.tipe_id=tipe_meja.tipe_id AND meja.tipe_id='$tipe_id'");
         } else {
-          $query = mysqli_query($koneksi, "SELECT * FROM meja WHERE status='on'");
+          $query = mysqli_query($koneksi, "SELECT meja.*, tipe_meja.tipe FROM meja JOIN tipe_meja ON meja.tipe_id=tipe_meja.tipe_id");
         }
 
         $no=1;
@@ -40,12 +40,14 @@
                   </a>
                   <div class='keterangan'>
                     <p>Kapasitas $row[kapasitas]</p>
+                    <p>Tipe : $row[tipe]</p>
                     <span>Status : $row[status]</span>
                   </div>
                   <div class='reserve'>
                     <a href='".BASE_URL."reserve_table.php?meja_id=$row[meja_id]'>RESERVE</a>
                   </div>
                 </li>";
+
           $no++;
         }
        ?>

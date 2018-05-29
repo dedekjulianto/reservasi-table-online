3dbdd58eac62bd29c8a502545701513d4e51af58
Dedek Julianto <dedekproject@gmail.com>
2018-05-30 02:00:45 +0700

    proses reserve succes

--- proses_pemesan.php
+++ proses_pemesan.php
@@ -12,6 +12,25 @@
   $guest_id = $_SESSION['guest_id'];
   $waktu_saat_ini = date("Y-m-d H:i:s");
 
-  $query = mysqli_query($koneksi, "INSERT INTO pesanan (nama_pemesan, nomor_telepon, jumlah, alamat, tanggal_pesan)
-                                               VALUES ('$nama_pemesan', '$nomor_telepon', '$jumlah', '$alamat')" );
+  $query = mysqli_query($koneksi, "INSERT INTO pesanan (nama_pemesan, guest_id, nomor_telepon, jumlah, alamat, tanggal_pesan)
+                                               VALUES ('$nama_pemesan', '$guest_id', '$nomor_telepon', '$jumlah', '$alamat', '$waktu_saat_ini')");
+
+  if ($query) {
+    $last_pesanan_id = mysqli_insert_id($koneksi);
+
+    $reserve = $_SESSION['reserve'];
+
+    foreach ($reserve as $key => $value) {
+      $meja_id = $key;
+      $nomor = $value['nomor'];
+      $kapasitas = $value['kapasitas'];
+      $tipeMeja = $value['tipe'];
+
+      mysqli_query($koneksi, "INSERT INTO invoice (reserve_id, meja_id, nomor, kapasitas, tipe)
+                                          VALUES ('$last_pesanan_id', '$meja_id', '$nomor', '$kapasitas', '$tipeMeja')");
+    }
+    unset($_SESSION["reserve"]);
+    header("location:".BASE_URL."index.php?page=myprofile&module=reserve&action=detail&reserve_id=$last_pesanan_id");
+  } 
+
 ?>
